269098550c8b8eaa7be106cd0eb26d9ec3544737
artur893 <arthal893@gmail.com>
2022-12-28 12:21:08 +0100

    Init

---
+++ racetrack/src/App.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class App extends Component {
+  render() {
+    return (
+      <h1 className="text-3xl font-bold underline">
+        Montrey chuj
+      </h1>
+    )
+  }
+}
+
+export default App
